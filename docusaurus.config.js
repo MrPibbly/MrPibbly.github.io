eccab27c7c9c4173c98fdee72fa298a6b2e8e68b
MrPibbly <MrPibbly@gmail.com>
2024-03-17 23:02:28 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'MrPibbly', // Usually your GitHub org/user name.
   projectName: 'MrPibbly.github.io', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
