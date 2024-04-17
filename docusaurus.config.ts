85907dde6c8a3da8526abc7c6118887217477dbf
MrPibbly <MrPibbly@gmail.com>
2024-04-17 00:52:35 -0500

    More
    More

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,58 +58,57 @@ const config: Config = {
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
+            href: 'https://github.com/MrPibbly/MrPibbly.github.io/',
+            label: 'Contribute to the Wiki!',
+            position: 'right',
+          },
       ],
     },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Swyft Minecraft Network`,
-    },
+   // footer: {
+     // style: 'dark',
+     // links: [
+       // {
+         // title: 'Docs',
+         // items: [
+           // {
+             // label: 'Tutorial',
+             // to: '/docs/intro',
+           // },
+         // ],
+       // },
+       // {
+         // title: 'Community',
+         // items: [
+           // {
+             // label: 'Stack Overflow',
+             // href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+           // },
+           // {
+             // label: 'Discord',
+             // href: 'https://discordapp.com/invite/docusaurus',
+           // },
+           // {
+             // label: 'Twitter',
+             // href: 'https://twitter.com/docusaurus',
+           // },
+         // ],
+       // },
+       // {
+         // title: 'More',
+         // items: [
+           // {
+             // label: 'Blog',
+             // to: '/blog',
+           // },
+           // {
+             // label: 'GitHub',
+             // href: 'https://github.com/facebook/docusaurus',
+           // },
+         // ],
+       // },
+     // ],
+     // copyright: `Copyright © ${new Date().getFullYear()} Swyft Minecraft Network`,
+   // },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
